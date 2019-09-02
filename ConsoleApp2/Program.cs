53d68e7952840f61f8a4c82966c98726a0062909
Mehad Hassan <mehad.hassan@flairstech.com>
2019-09-02 12:53:36 +0200

    add project files

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 18;
+            int z = y / x;
+
+            Console.WriteLine(z);
+        }
+    }
+}
